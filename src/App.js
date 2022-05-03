45594fc2380df502a7ed9052ec14c4b760325e08
Severus7 <joshuasapz@gmail.com>
2022-05-03 13:34:20 +0800

    Added rapidAPI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 /* 
@@ -51,6 +51,52 @@ const TemperatureAverage = (props) => {
   return (
     <div>
       The average <b>{avg.toFixed(2)}</b> degress Fahrenheit.
+    </div>
+  );
+};
+
+const AddCityButton = (props) => {
+  const context = React.useContext(WeatherContext);
+
+  const [name, setName] = React.useState("");
+
+  const submit = () => {
+    const unit = "imperial";
+    const mode = "json";
+    const encodedName = encodeURIComponent(name);
+
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
+        "X-RapidAPI-Key": "40de412817msh85793b39f58a5e6p1cc670jsnca3120d080bd",
+      },
+    };
+
+    fetch(
+      "https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=imperial&mode=xml",
+      options
+    )
+      .then((response) => {
+        console.log(response);
+        if (response.status !== 200) throw new Error();
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        context.addCity(name, json.main.temp);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div className="add-city-form">
+      <input
+        className="input"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button className="input">Add</button>
     </div>
   );
 };
